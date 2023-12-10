7ceb87f8dd4d3c47be93beb53a7be97ccd54e769
zicchi <zidanezicchi@gmail.com>
2023-12-10 20:02:07 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,6 +11,21 @@ root.render(
   </React.StrictMode>
 );
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    }).catch(function(err) {
+      console.log(err)
+    });
+  });
+} else {
+  console.log('service worker is not supported');
+}
+
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
